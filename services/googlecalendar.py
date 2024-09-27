f755ba30a6cb51512ce11ef341f2a969eef9105d
antONget <a.l.ponomare@mail.ru>
2024-09-27 09:16:00 +0300

    интеграция календарь

--- services/googlecalendar.py
+++ services/googlecalendar.py
@@ -5,14 +5,17 @@ from googleapiclient.discovery import build
 
 
 
-calendarId = 'YS5sLnBvbm9tYXJldjE5ODlAZ21haWwuY29t8@group.calendar.google.com'
-SERVICE_ACCOUNT_FILE = 'services/credentials.json'
+# calendarId = 'YS5sLnBvbm9tYXJldjE5ODlAZ21haWwuY29t8@group.calendar.google.com'
+# SERVICE_ACCOUNT_FILE = 'credentials.json'
 
 
 class GoogleCalendar(object):
-    CALENDAR_ID = 'a.l.ponomarev1989@gmail.com'
+    # CALENDAR_ID = 'a.l.ponomarev1989@gmail.com'
+    CALENDAR_ID = 'detox.tour.turkey@gmail.com'
     SCOPES = ['https://www.googleapis.com/auth/calendar']
-    FILE_PATH = 'services/credentials.json'
+    # FILE_PATH = 'calendar_google.json'
+    FILE_PATH = 'credentials.json'
+
 
     def __init__(self):
         credentials = service_account.Credentials.from_service_account_file(
@@ -60,13 +63,16 @@ class GoogleCalendar(object):
 
 calendarG = GoogleCalendar()
 
-# pprint.pprint(obj.get_calendar_list())
+# pprint.pprint(calendarG.get_calendar_list())
 # calendar_id = 'a.l.ponomarev1989@gmail.com'
-# obj.add_calendar(calendar_id='a.l.ponomarev1989@gmail.com')
+# calendarG.add_calendar(calendar_id='detox.tour.turkey@gmail.com')
 
 # time_dict = {"H1": 14, "M1": 40, "H2": 15, "M2": 20}
-# obj.create_event(time_dict=time_dict, summary='Записи на консультацию Тестовая', description='Проверка работы')
-# pprint.pprint(obj.get_calendar_list())
+# calendarG.create_event(time_dict=time_dict,
+#                        summary='Записи на консультацию Тестовая',
+#                        description='Проверка работы',
+#                        data_event='2024-09-26')
+# pprint.pprint(calendarG.get_calendar_list())
 # event = obj.get_event(data='2024-09-22')
 # print(event)
 
